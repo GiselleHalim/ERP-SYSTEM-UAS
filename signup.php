dd605804eaa5afa2ee6150ae1b6919fe6cd6dff8
GiselleHalim <96042766+GiselleHalim@users.noreply.github.com>
2022-01-10 22:35:55 +0700

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sign Up</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/login.css">
+</head>
+<body>
+
+<style>
+body {
+  background-color: black;
+}
+</style>
+
+     <form action="signup-check.php" method="post">
+     	<h2>Sign Up</h2>
+
+
+          <label>Name</label>
+                         <input type="text"
+                      name="name"
+                      placeholder="Name"><br>
+
+
+											<label>User Name</label>
+											<?php if (isset($_GET['uname'])) { ?>
+													 <input type="text"
+																	name="uname"
+																	placeholder="User Name"
+																	value="<?php echo $_GET['uname']; ?>"><br>
+											<?php }else{ ?>
+													 <input type="text"
+																	name="uname"
+																	placeholder="User Name"><br>
+											<?php }?>
+											
+     	<label>Password</label>
+     	<input type="password" name="password" placeholder="Password" id="password"><br>
+
+          <label>Re-enter your password</label>
+          <input type="password"
+                 name="re_password"
+                 placeholder="Password" id="re_password">
+           		  <script>
+           								 function myFunction() {
+             							 var x = document.getElementById("password");
+                           var y = document.getElementById("re_password");
+             						 if (x.type === "password" && y.type === "password") {
+               				 x.type = "text";
+                       y.type = "text;"
+             			 } else {
+               	 x.type = "password";
+                 y.type = "password";
+             	}
+           	}
+           </script>
+
+     	<button type="submit">Sign Up</button>
+          <a href="index.php">Already have an account?</a>
+     </form>
+</body>
+</html>
